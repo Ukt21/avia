f716ed78af2aa9f7a8cf11d063eb743d3ea5ee0b
Ukt21 <141407148+Ukt21@users.noreply.github.com>
2025-10-28 21:10:43 +0500

    Update main.py

--- arzonuching/app/main.py
+++ arzonuching/app/main.py
@@ -294,9 +294,8 @@ def build_results_text(q: QueryState) -> str:
 
     return "\n".join(head_lines + lines) + "\n"
 
-
     return "
-".join(head_lines + lines) + "
+             ".join(head_lines + lines) + "
 "
 
 # =============================
